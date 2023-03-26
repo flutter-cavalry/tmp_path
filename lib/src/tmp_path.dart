c2edd627a5b10530a2113f5fe1fdcf602ddddb59
Mgen <m@mgenware.com>
2023-03-26 15:41:48 +0800

    No default prefix

--- lib/src/tmp_path.dart
+++ lib/src/tmp_path.dart
@@ -6,12 +6,12 @@ var uuid = Uuid();
 
 /// Returns a temporary file name.
 String tmpFileName() {
-  return '${uuid.v4().replaceAll('-', '')}_${DateTime.now().millisecondsSinceEpoch}';
+  return '${uuid.v4().replaceAll('-', '')}${DateTime.now().millisecondsSinceEpoch}';
 }
 
 /// Returns a temporary path. You can use that to create a temporary file or directory.
 String tmpPath({String prefix = '', String? parentDirectory}) {
   var parent = parentDirectory ?? Directory.systemTemp.path;
   var tmpName = tmpFileName();
-  return p.join(parent, '${prefix.isEmpty ? 'tmp' : prefix}_$tmpName');
+  return p.join(parent, '${prefix.isEmpty ? '' : prefix}$tmpName');
 }
--- test/tmp_path_test.dart
+++ test/tmp_path_test.dart
@@ -5,15 +5,22 @@ import 'package:test/test.dart';
 import 'package:tmp_path/src/tmp_path.dart';
 
 void main() {
-  test('tmp path', () {
-    var sysTmp = Directory.systemTemp.path;
-    var tmpDir = tmpPath();
-    expect(tmpDir.startsWith(p.join(sysTmp, 'tmp_')), true);
+  test('Tmp path', () {
+    var sysTmpDir = Directory.systemTemp.path;
+    var tmp = tmpPath();
+    expect(p.dirname(tmp), sysTmpDir);
+    expect(p.basename(tmp).isNotEmpty, true);
+    expect(p.basename(tmp).contains('-'), false);
   });
 
-  test('with prefix', () {
-    var sysTmp = Directory.systemTemp.path;
-    var tmpDir = tmpPath(prefix: 'p');
-    expect(tmpDir.startsWith(p.join(sysTmp, 'p_')), true);
+  test('Prefix', () {
+    var tmp = tmpPath(prefix: 'p_');
+    expect(p.basename(tmp).startsWith('p_'), true);
+  });
+
+  test('Parent dir', () {
+    var parentDir = '/a/b/cc';
+    var tmp = tmpPath(parentDirectory: parentDir);
+    expect(p.dirname(tmp), parentDir);
   });
 }
